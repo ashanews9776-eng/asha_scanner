fe60169a6aecdf0cb0bacd063a3f86ddc3274078
ashanews9776-eng <ashanews9776@gmail.com>
2026-07-03 07:37:24 +0330

    build: bump version to 0.4 (v2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.ahoura.asha_scanner_ip"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.3"
+        versionCode = 2
+        versionName = "0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
