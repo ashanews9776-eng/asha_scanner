507d3c608832edf0f0d09f4cf3eb9118d8542e14
ashanews9776-eng <ashanews9776@gmail.com>
2026-07-03 08:05:57 +0330

    build: synchronize version name with release tag 0.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.ahoura.asha_scanner_ip"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.5"
+        versionCode = 4
+        versionName = "0.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
