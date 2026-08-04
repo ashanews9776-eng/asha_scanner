80c830d12660ca2f448b13dea101768cb9786507
ashanews9776-eng <ashanews9776@gmail.com>
2026-08-04 17:13:33 +0330

    Bump version to v0.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.ahoura.asha_scanner_ip"
         minSdk = 24
         targetSdk = 37
-        versionCode = 8
-        versionName = "0.6.3"
+        versionCode = 9
+        versionName = "0.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
