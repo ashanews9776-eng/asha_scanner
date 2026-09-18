4210fe00c0114d6f5dcb66c080ceb184e531c4f6
ashanews9776-eng <ashanews9776@gmail.com>
2026-09-18 13:18:58 +0330

    chore: bump version to 0.8.2 (versionCode 13)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.ahoura.asha_scanner_ip"
         minSdk = 26
         targetSdk = 37
-        versionCode = 12
-        versionName = "0.8.1"
+        versionCode = 13
+        versionName = "0.8.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
